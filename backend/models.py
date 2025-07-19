4329283a7d9cab7c4763829d26ae209a4bf73a63
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2025-07-19 16:49:20 +0100

    added test table model for testing

--- backend/models.py
+++ backend/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
     def set_password(self, password):
@@ -25,3 +25,7 @@ class Movie(db.Model):
     release_year = db.Column(db.Integer, nullable=False)
     genre = db.Column(db.String(50), nullable=False)
     video_url = db.Column(db.String(255), nullable=True)
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
