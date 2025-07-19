2babce482b53bc347bd4e66ab78d6f9769b1039e
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2025-07-19 16:50:11 +0100

    intialized the app file

--- backend/app.py
+++ backend/app.py
@@ -2,21 +2,30 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from routes.upload import upload
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://gabby:gabriel@localhost/gabflix_db'
-app.config['JWT_SECRET_KEY'] = 'your_secret_key'
-app.register_blueprint(upload, url_prefix='/api')
-
-db = SQLAlchemy(app)
-jwt = JWTManager(app)
-CORS(app)
-
-@app.route('/')
-def home():
-    return jsonify({'message': "Welcome to the Gabflix API 🚀"})
+from flask_migrate import Migrate
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+db = SQLAlchemy()
+jwt = JWTManager()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://gabby:gabriel@localhost/gabflix_db'
+    app.config['JWT_SECRET_KEY'] = 'your_secret_key'
+
+    db.init_app(app)
+    jwt.init_app(app)
+    CORS(app)
+    migrate.init_app(app, db)
+
+    from routes.upload import upload
+    from models import User, Movie, Test
+    app.register_blueprint(upload, url_prefix='/api')
+
+
+    @app.route('/')
+    def home():
+        return jsonify({'message': "Welcome to the Gabflix API 🚀"})
+
+    return app
