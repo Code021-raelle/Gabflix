e34cb3e8b0723af1185981a49c460a50d9aa7c39
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2025-08-10 09:11:53 +0100

    added createprofile and protectedroute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,10 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Watch from './pages/Watch';
+import Upload from './pages/Upload';
+import Profiles from './pages/Profiles';
+import CreateProfile from './pages/CreateProfile';
+import ProtectedRoute from './components/ProtectedRoute';
 import './App.css'
 
 function App() {
@@ -12,9 +16,24 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/profiles" element={<Profiles />} />
         <Route path="/login" element={<Login />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path="/watch/:id" element={<Watch />} />
+        <Route path="/watch/:id" element={
+          <ProtectedRoute>
+            <Watch />
+          </ProtectedRoute>
+        } />
+        <Route path="/upload" element={
+          <ProtectedRoute>
+            <Upload />
+          </ProtectedRoute>
+        } />
+        <Route path="/create-profile" element={
+          <ProtectedRoute>
+            <CreateProfile />
+          </ProtectedRoute>
+        } />
       </Routes>
     </div>
   );
